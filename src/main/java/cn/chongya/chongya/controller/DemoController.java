d23569803343eac6c1fdecbe9342c926f41c8f17
ashuai <502123065@qq.com>
2020-07-08 19:37:13 +0800

    zx
    demo test

--- src/main/java/cn/chongya/chongya/controller/DemoController.java
+++ src/main/java/cn/chongya/chongya/controller/DemoController.java
@@ -1,6 +1,7 @@
 package cn.chongya.chongya.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,12 @@ public class DemoController {
     @GetMapping("/test")
     public String test() {
         return "123";
+    }
+
+
+
+    @PostMapping("/getUserName")
+    public String getUserName() {
+        return "ashuai";
     }
 }
