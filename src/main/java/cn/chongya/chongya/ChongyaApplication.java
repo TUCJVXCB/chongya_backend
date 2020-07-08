6b3173d31d4b0c068e996f42d68de5b9a5f70840
TUCJVXCB <838806482@qq.com>
2020-07-08 19:16:25 +0800

    init

---
+++ src/main/java/cn/chongya/chongya/ChongyaApplication.java
@@ -0,0 +1,13 @@
+package cn.chongya.chongya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChongyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChongyaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/chongya/chongya/ChongyaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chongya.chongya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChongyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
